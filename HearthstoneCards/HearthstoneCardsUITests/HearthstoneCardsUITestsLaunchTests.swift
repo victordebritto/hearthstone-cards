de452b3c70fd69305d34fa5a3da68811f94c10b6
Victor Britto e Barros <victorbritto@outlook.com>
2022-10-25 10:47:31 -0400

    Create xcode project

---
+++ HearthstoneCards/HearthstoneCardsUITests/HearthstoneCardsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HearthstoneCardsUITestsLaunchTests.swift
+//  HearthstoneCardsUITests
+//
+//  Created by Victor Britto e Barros on 25/10/22.
+//
+
+import XCTest
+
+final class HearthstoneCardsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
